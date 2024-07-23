3b16fc5e2cf6e9de150374a03d6414c4b06be6ca
Adam S <23557702@sun.ac.za>
2024-07-23 17:04:04 +0200

    Moved the extraction script to a regular py file and into the right folder

--- recorded_odometry_publisher_py/Bags/ExtractOdomData.py
+++ recorded_odometry_publisher_py/Bags/ExtractOdomData.py
@@ -2,7 +2,7 @@ import bagpy
 from bagpy import bagreader
 import pandas as pd
 
-b = bagreader("2024-07-17-10-14-34.bag")
+b = bagreader("Odometry.bag")
 # b.topic_table
 
 odometryMsgs = b.odometry_data()
