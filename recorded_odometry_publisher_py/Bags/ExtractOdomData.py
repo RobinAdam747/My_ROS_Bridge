9ad359a85321a01a85a801b6c5b40d81f7b25a51
Adam S <23557702@sun.ac.za>
2024-07-23 17:09:03 +0200

    Cleaned and commented

--- recorded_odometry_publisher_py/Bags/ExtractOdomData.py
+++ recorded_odometry_publisher_py/Bags/ExtractOdomData.py
@@ -1,12 +1,15 @@
-import bagpy
-from bagpy import bagreader
+import bagpy    # BagPy is the utility to manipulate rosbags
+from bagpy import bagreader # BagReader is the utility to extract the csv from the rosbag
 import pandas as pd
 
+# Create a bagreader object
 b = bagreader("Odometry.bag")
 # b.topic_table
 
+# Extract the odometry messages
 odometryMsgs = b.odometry_data()
 # odometryMsgs
 
+# Create csv of the odometry messages
 odometryData = pd.read_csv(odometryMsgs[0])
 # odometryData
