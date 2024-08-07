a9e9be98c27ad3f11ece130cee8a048f13057044
Adam S <23557702@sun.ac.za>
2024-08-08 01:57:57 +0200

    Using duration parameter to stop start recorder

--- bridge_start_ROS1_RecorderExtractor.sh
+++ bridge_start_ROS1_RecorderExtractor.sh
@@ -4,12 +4,7 @@ source /opt/ros/noetic/setup.bash
 
 while true; do
 	# start the rosbag recording
-	rosbag record -O Odometry.bag /RosAria/pose
-	
-	# wait a bit
-	
-	# stop the recording
-	pkill -f "rosbag record -O Odometry.bag /RosAria/pose"
+	rosbag record --duration=0.3 -O Odometry.bag /RosAria/pose
 
 	# Run recorder-extractor script:
 	python3 /home/esl/My_ROS_Bridge/recorded_odometry_publisher_py/RecordAndExtractOdomData.py
