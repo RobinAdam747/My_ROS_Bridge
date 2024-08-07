cc118130068675f14f1c305c3f99c45be2b33edb
Adam S <23557702@sun.ac.za>
2024-08-08 01:34:10 +0200

    Python once again causing grief, bash all the way

--- bridge_start_ROS1_RecorderExtractor.sh
+++ bridge_start_ROS1_RecorderExtractor.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 # Startup of bridge (ROS1)
-# Run recorder-extractor script:
-python /home/esl/My_ROS_Bridge/recorded_odometry_publisher_py/RecordAndExtractOdomData.py
+source /opt/ros/noetic/setup.bash
+
+while true; do
+	# start the rosbag recording
+	rosbag record -O Odometry.bag /RosAria/pose
+	
+	# wait a bit
+	
+	# stop the recording
+	pkill -f "rosbag record -O Odometry.bag /RosAria/pose"
+
+	# Run recorder-extractor script:
+	python /home/esl/My_ROS_Bridge/recorded_odometry_publisher_py/RecordAndExtractOdomData.py
--- recorded_odometry_publisher_py/RecordAndExtractOdomData.py
+++ recorded_odometry_publisher_py/RecordAndExtractOdomData.py
@@ -20,30 +20,41 @@ def stop_recording(process):
     process.wait()
 
 def main():
-    interval = 0.1  # Time in seconds to record
+
+    # Create a bagreader object
+    b = bagreader(bagfile="Bags/Odometry.bag")
+
+    # Extract the odometry messages 
+    odometryMsgs = b.message_by_topic(topic="/RosAria/pose")
+
+    # Create csv of the odometry messages
+    odometryData = pd.read_csv(odometryMsgs[0])
+
+
+    # interval = 0.1  # Time in seconds to record
     # bag_counter = 0
 
-    while True:
-        bag_name = "Odometry.bag"
-        print(f"Starting recording: {bag_name}")
-        process = start_recording(bag_name)
+    # while True:
+    #     bag_name = "Odometry.bag"
+    #     print(f"Starting recording: {bag_name}")
+    #     process = start_recording(bag_name)
         
-        time.sleep(interval)
+    #     time.sleep(interval)
         
-        print(f"Stopping recording: {bag_name}")
-        stop_recording(process)
+    #     print(f"Stopping recording: {bag_name}")
+    #     stop_recording(process)
         
-        # bag_counter += 1
-        # time.sleep(1)  # Wait before starting the next recording
+    #     # bag_counter += 1
+    #     # time.sleep(1)  # Wait before starting the next recording
 
-        # Create a bagreader object
-        b = bagreader(bagfile="Bags/Odometry.bag")
+    #     # Create a bagreader object
+    #     b = bagreader(bagfile="Bags/Odometry.bag")
 
-        # Extract the odometry messages 
-        odometryMsgs = b.message_by_topic(topic="/RosAria/pose")
+    #     # Extract the odometry messages 
+    #     odometryMsgs = b.message_by_topic(topic="/RosAria/pose")
 
-        # Create csv of the odometry messages
-        odometryData = pd.read_csv(odometryMsgs[0])
+    #     # Create csv of the odometry messages
+    #     odometryData = pd.read_csv(odometryMsgs[0])
 
 if __name__ == "__main__":
     main()
