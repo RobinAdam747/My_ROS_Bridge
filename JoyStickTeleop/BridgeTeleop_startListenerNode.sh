f248cabbc0d344829d8718b318ee230c1723c9d1
Adam S <23557702@sun.ac.za>
2024-08-18 20:32:52 +0200

    forgot a bash file

---
+++ JoyStickTeleop/BridgeTeleop_startListenerNode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run node that listens to the ROS2 joy topic and sends the data to the a csv file
+python3 /home/administrator/My_ROS_Bridge/JoyStickTeleop/JoyListenSaveToCSV_node.py
