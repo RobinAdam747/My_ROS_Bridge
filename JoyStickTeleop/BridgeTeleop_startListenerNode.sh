4753e60c3bc4e5bb612c37ce35a3cad7ec8b55e2
Adam S <23557702@sun.ac.za>
2024-08-18 20:33:55 +0200

    fixed path

--- JoyStickTeleop/BridgeTeleop_startListenerNode.sh
+++ JoyStickTeleop/BridgeTeleop_startListenerNode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run node that listens to the ROS2 joy topic and sends the data to the a csv file
-python3 /home/administrator/My_ROS_Bridge/JoyStickTeleop/JoyListenSaveToCSV_node.py
+python3 /home/esl/colcon_ws/src/My_ROS_Bridge/JoyStickTeleop/JoyListenSaveToCSV_node.py
