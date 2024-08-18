8c97e56cda7833fc0071dc2860e79b1bed6f9afa
Adam S <23557702@sun.ac.za>
2024-08-18 19:21:18 +0200

    path fix

--- bridge_start_ROS1_RecorderExtractor.sh
+++ bridge_start_ROS1_RecorderExtractor.sh
@@ -7,5 +7,5 @@ while true; do
 	rosbag record --duration=0.3 -O Odometry.bag /RosAria/pose
 
 	# Run recorder-extractor script:
-	python3 /home/esl/My_ROS_Bridge/recorded_odometry_publisher_py/RecordAndExtractOdomData.py
+	python3 /home/noeticpioneer/My_ROS_Bridge/recorded_odometry_publisher_py/RecordAndExtractOdomData.py
 done
