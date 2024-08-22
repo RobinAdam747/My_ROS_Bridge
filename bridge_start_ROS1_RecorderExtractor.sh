84209bbfdc124b48be1580a3f6cc238b2f856d63
Adam S <23557702@sun.ac.za>
2024-08-22 12:06:50 +0200

    don't touch things that WORK

--- bridge_start_ROS1_RecorderExtractor.sh
+++ bridge_start_ROS1_RecorderExtractor.sh
@@ -4,7 +4,7 @@ source /opt/ros/noetic/setup.bash
 
 while true; do
 	# start the rosbag recording
-	rosbag record --duration=0.3 -O /home/noeticpioneer/My_ROS_Bridge/recorded_odometry_publisher_py/Bags/Odometry.bag /RosAria/pose
+	rosbag record --duration=0.3 -O Odometry.bag /RosAria/pose
 
 	# Run recorder-extractor script:
 	python3 /home/noeticpioneer/My_ROS_Bridge/recorded_odometry_publisher_py/RecordAndExtractOdomData.py
