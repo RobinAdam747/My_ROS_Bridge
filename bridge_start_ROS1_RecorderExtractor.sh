c97e6db50b415845cf3e7e3fa909d3391d82b5fc
Adam S <23557702@sun.ac.za>
2024-08-22 14:48:18 +0200

    now records both odom and tf topics

--- bridge_start_ROS1_RecorderExtractor.sh
+++ bridge_start_ROS1_RecorderExtractor.sh
@@ -4,7 +4,7 @@ source /opt/ros/noetic/setup.bash
 
 while true; do
 	# start the rosbag recording
-	rosbag record --duration=0.3 -O Odometry.bag /RosAria/pose
+	rosbag record --duration=0.3 -O OdometryAndTF.bag /RosAria/pose /tf
 
 	# Run recorder-extractor script:
 	python3 /home/noeticpioneer/My_ROS_Bridge/recorded_odometry_publisher_py/RecordAndExtractOdomData.py
