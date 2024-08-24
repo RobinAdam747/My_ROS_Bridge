ea48744e48c4d62075976c26f42a7bd3de7c3711
RobinAdam747 <adam.sendzul@gmail.com>
2024-08-24 16:25:36 +0200

    python3 apparently

--- JoyStickTeleop/BridgeTeleop_startROS1node.sh
+++ JoyStickTeleop/BridgeTeleop_startROS1node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run node that publishes the csv joy data to the ROS1 husky topic
-python /home/noeticpioneer/My_ROS_Bridge/JoyStickTeleop/JoyDataPublisher_node.py
+python3 /home/noeticpioneer/My_ROS_Bridge/JoyStickTeleop/JoyDataPublisher_node.py
