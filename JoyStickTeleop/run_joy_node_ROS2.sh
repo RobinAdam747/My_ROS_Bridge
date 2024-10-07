7a14232aa76fad70ea6bb59585a6deb8e54bb214
Adam S <23557702@sun.ac.za>
2024-10-07 17:38:08 +0200

    WIP of headless teleop

---
+++ JoyStickTeleop/run_joy_node_ROS2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Setup for joystick
+# joystick node to get messages from controller
+source /opt/ros/humble/setup.bash
+ros2 run joy joy_node
---
+++ JoyStickTeleop/run_joy_transfer_ROS2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/ros/humble/setup.bash
+python3 /home/eslminingnuc/My_ROS_Bridge/JoyStickTeleop/joystick_teleop_husky_ROS2.py
